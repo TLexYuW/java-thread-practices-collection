7a1268635e9cdfa4d053f73b003abec04934947c
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-05-07 18:32:02 +0800

    update

---
+++ juc-advanced-atguigu/src/main/java/_20/mallDemo/CompletableFutureMallDemo.java
@@ -0,0 +1,27 @@
+package _20.mallDemo;
+
+import lombok.Getter;
+
+/**
+ * @author : Lex Yu
+ */
+public class CompletableFutureMallDemo {
+
+	public static void main(String[] args) {
+
+	}
+}
+
+
+@Getter
+class NetMall{
+
+	private String netMallName;
+
+	public NetMall() {
+	}
+
+	public NetMall(String netMallName) {
+		this.netMallName = netMallName;
+	}
+}
